9ff1d362c6bbcb8eaf2b10b3adfb7e4b86cb7605
SuaceX <mahmoudzak4@gmail.com>
2022-11-29 16:59:04 +0300

    prototype-v1 (basic layout w/ table)

--- src/main/java/com/course/offering/App.java
+++ src/main/java/com/course/offering/App.java
@@ -5,16 +5,22 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.channels.SocketChannel;
 
 public class App extends Application {
 
-    // Panes
     private BorderPane root;
 
     // Basics
@@ -22,68 +28,157 @@ public class App extends Application {
     private Scene scene;
 
     // Margin control for buttons
-    private Insets INSETS = new Insets(5, 5, 5, 5);
+    private Insets General_INSETS = new Insets(5, 5, 5, 5);
+    private Insets Button_INSETS = new Insets(5, 5, 5, 5);
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         this.primaryStage = primaryStage;
-        // Image image = new Image("sample1.jpg", 60, 60, true, true);
-        // ImageView sampleImage = new ImageView(image);
-        // Button button = new Button("Close", sampleImage);
-        // button.setMinHeight(80);
-        // button.setMinWidth(140);
-        // StackPane pane = new StackPane();
-        // pane.getChildren().add(button);
-        // button.setOnAction(e -> primaryStage.close());
-        // borderPane.setStyle("-fx-background-color: #000");
 
         initUi();
     }
 
-    private void initUi() {
-        // Setting panes
-        root = new BorderPane();
-        VBox basketPane = new VBox();
-        HBox bottomPane = new HBox();
-        VBox mainPane = new VBox();
+    private void setupMainPane() {
+        // Components smallest to largest
+        TableView table = new TableView<String>();
+        AnchorPane mainPane = new AnchorPane(table);
 
-        // Testing buttons to fill up space
-        Button centerButton = createButton("Center");
-        Button rightButton = createButton("Right");
-        Button bottomButton = createButton("Bottom");
+        createTableColumns(table);
 
-        mainPane.getChildren().setAll(centerButton);
-        basketPane.getChildren().setAll(rightButton);
+        TableRow<String> sevenAM = new TableRow<>();
+
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        // table.getItems().add(new String("Buggs"));
+        // table.getItems().add(new String("Daffy", "Duck", 83));
+        // table.getItems().add(new String("Foghorn", "Leghorn", 74));
+        // table.getItems().add(new String("Elmer", "Fudd", 83));
+        // table.getItems().add(new String("Tweety", "Bird", 73));
+
+        AnchorPane.setTopAnchor(table, 0.0);
+        AnchorPane.setBottomAnchor(table, 0.0);
+        AnchorPane.setLeftAnchor(table, 0.0);
+        AnchorPane.setRightAnchor(table, 0.0);
+        mainPane.setMaxWidth(Double.MAX_VALUE);
+        mainPane.setMaxHeight(Double.MAX_VALUE);
+
+        mainPane.getChildren().setAll(table);
+
+        BorderPane.setMargin(mainPane, General_INSETS);
+        root.setCenter(mainPane);
+    }
+
+    private void createTableColumns(TableView table) {
+        // Sample for later
+        // TableColumn<Person, Integer> ageColumn = new TableColumn<Person,
+        // Integer>("Age");
+        // ageColumn.setCellValueFactory(new PropertyValueFactory<Person,
+        // Integer>("age"));
+
+        TableColumn<String, String> sunday = new TableColumn<String, String>("Sunday");
+        sunday.setCellValueFactory(new PropertyValueFactory<String, String>("firstName"));
+
+        TableColumn<String, String> monday = new TableColumn<String, String>("Monday");
+        monday.setCellValueFactory(new PropertyValueFactory<String, String>("lastName"));
+
+        TableColumn<String, String> tuesday = new TableColumn<String, String>("Tuesday");
+        tuesday.setCellValueFactory(new PropertyValueFactory<String, String>("lastName"));
+
+        TableColumn<String, String> wednesday = new TableColumn<String, String>("Wednesday");
+        wednesday.setCellValueFactory(new PropertyValueFactory<String, String>("lastName"));
+
+        TableColumn<String, String> thursday = new TableColumn<String, String>("Thursday");
+        thursday.setCellValueFactory(new PropertyValueFactory<String, String>("lastName"));
+
+        TableColumn<String, String> friday = new TableColumn<String, String>("Friday");
+        friday.setCellValueFactory(new PropertyValueFactory<String, String>("lastName"));
+
+        TableColumn<String, String> saturday = new TableColumn<String, String>("Saturday");
+        saturday.setCellValueFactory(new PropertyValueFactory<String, String>("lastName"));
+
+        table.getColumns().add(sunday);
+        table.getColumns().add(monday);
+        table.getColumns().add(tuesday);
+        table.getColumns().add(wednesday);
+        table.getColumns().add(thursday);
+        table.getColumns().add(friday);
+        table.getColumns().add(saturday);
+    }
+
+    private void setupBottomPane() {
+        HBox bottomPane = new HBox(10);
+        Button bottomButton = createBasicButton("Save Schedule", 150, 35);
+
         bottomPane.getChildren().setAll(bottomButton);
 
-        // Main Pane Setup
-
-        // Right Pane Setup
-        VBox.setMargin(rightButton, INSETS);
-        basketPane.setStyle("-fx-background-color: #f34839");
-        basketPane.setAlignment(Pos.CENTER);
-
         // Bottom Pane Setup
-        HBox.setMargin(bottomButton, INSETS);
+        HBox.setMargin(bottomButton, General_INSETS);
         bottomPane.setAlignment(Pos.CENTER_LEFT);
         bottomPane.setStyle("-fx-background-color: #eb8d13");
 
-        root.setCenter(mainPane);
-        root.setRight(basketPane);
+        BorderPane.setMargin(bottomButton, General_INSETS);
         root.setBottom(bottomPane);
 
-        scene = new Scene(root, 600, 400);
+    }
+
+    private void initUi() {
+        root = new BorderPane();
+
+        // The pane on the right with all courses
+        setupBasketPane();
+        // The schedule pane
+        setupMainPane();
+        // The saving button pane
+        setupBottomPane();
+
+        scene = new Scene(root, 1200, 750);
         primaryStage.setTitle("Second Page (Prototype)");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private Button createButton(String text) {
+    private void setupBasketPane() {
+        // Adding components, smallest to largest
+        VBox basketItemsContainer = new VBox();
+        ScrollPane scrollPane = new ScrollPane(basketItemsContainer);
+        AnchorPane anchorPane = new AnchorPane(scrollPane);
+
+        // Styling
+        // Fill the AnchorPane with the ScrollPane and set the Anchors to 0.0
+        // That way the ScrollPane will take the full size of the Parent of
+        // the AnchorPane
+        AnchorPane.setTopAnchor(scrollPane, 0.0);
+        AnchorPane.setBottomAnchor(scrollPane, 0.0);
+        AnchorPane.setLeftAnchor(scrollPane, 0.0);
+        AnchorPane.setRightAnchor(scrollPane, 0.0);
+        anchorPane.setMaxWidth(Double.MAX_VALUE);
+        anchorPane.setMaxHeight(Double.MAX_VALUE);
+        anchorPane.setPrefWidth(300);
+
+        scrollPane.setMaxWidth(Double.MAX_VALUE);
+        scrollPane.setMaxHeight(Double.MAX_VALUE);
+        scrollPane.setFitToWidth(true);
+
+        basketItemsContainer.setAlignment(Pos.CENTER);
+        basketItemsContainer.setMaxWidth(Double.MAX_VALUE);
+        basketItemsContainer.setMaxHeight(Double.MAX_VALUE);
+
+        for (int i = 0; i < 40; i++) {
+            Button rightButton = createBasicButton("Right", 100, 70);
+            basketItemsContainer.getChildren().add(rightButton);
+        }
+
+        BorderPane.setMargin(anchorPane, General_INSETS);
+        root.setRight(anchorPane);
+    }
+
+    private Button createBasicButton(String text, double prefWidth, double prefHeight) {
         Button button = new Button(text);
         button.setMaxWidth(Double.MAX_VALUE);
         button.setMaxHeight(Double.MAX_VALUE);
-        button.setMinWidth(150);
-        BorderPane.setMargin(button, INSETS);
+        button.setPrefWidth(prefWidth);
+        button.setPrefHeight(prefHeight);
+        BorderPane.setMargin(button, Button_INSETS);
         BorderPane.setAlignment(button, Pos.CENTER);
         return button;
     }
